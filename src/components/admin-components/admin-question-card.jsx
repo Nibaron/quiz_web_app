7d635f93b92a91c4e2a93abb96d683ab67f9faee
Nibaron Kumar Das <nibaronkumar02@gmail.com>
2024-11-21 18:16:57 +0600

    quiz set entry page working with dummy data

---
+++ src/components/admin-components/admin-question-card.jsx
@@ -0,0 +1,41 @@
+
+export default function AdminQuestionCard({ index, question, options, correctAnswer, setSingleQuestion }) {
+
+    const handleEdit = () => {
+        setSingleQuestion({
+            question,
+            options,
+            correctAnswer,
+        });
+    };
+
+    return (
+        <div className="rounded-lg overflow-hidden shadow-sm mb-4">
+            <div className="bg-white p-6 !pb-2">
+                <div className="flex justify-between items-center mb-4">
+                    <h3 className="text-lg font-semibold">
+                        {`${index}. ${question}`}
+                    </h3>
+                </div>
+                <div className="space-y-2">
+                    {options.map((option, idx) => (
+                        <label key={idx} className="flex items-center space-x-3">
+                            <input
+                                type="radio"
+                                name={`answer${index}`}
+                                className="form-radio text-buzzr-purple"
+                                checked={option === correctAnswer}
+                                readOnly
+                            />
+                            <span>{option}</span>
+                        </label>
+                    ))}
+                </div>
+            </div>
+            <div className="flex space-x-4 bg-primary/10 px-6 py-2">
+                <button className="text-red-600 hover:text-red-800 font-medium">Delete</button>
+                <button className="text-primary hover:text-primary/80 font-medium" onClick={handleEdit}>Edit Question</button>
+            </div>
+        </div>
+    )
+}
--- src/pages/admin-pages/quiz-set-entry-page.jsx
+++ src/pages/admin-pages/quiz-set-entry-page.jsx
@@ -1,12 +1,24 @@
 import Sidebar from '../../components/admin-components/sidebar';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { QuizIdContext } from '../../context';
+import AdminQuestionCard from '../../components/admin-components/admin-question-card';
 
 
 export default function QuizSetEntryPage() {
     const { quizId } = useContext(QuizIdContext);
+    const [adminQuestionsData] = useState([{
+        "index": "1",
+        "question": "Which of the following is NOT a binary tree traversal method?",
+        "options": ["Inorder", "Preorder", "Postorder", "Crossorder"],
+        "correctAnswer": "Preorder",
+    }]);
+    const [singleQuestion, setSingleQuestion] = useState({
+        "question": "",
+        "options": ["", "", "", ""],
+        "correctAnswer": "#",
+    })
 
-    console.log(quizId);
+    console.log(singleQuestion);
 
     return (
         <main className="bg-[#F5F3FF] min-h-screen flex">
@@ -41,61 +53,57 @@ export default function QuizSetEntryPage() {
                             </p>
 
                             <div className="space-y-4">
-                                <h2 className="text-xl font-bold text-foreground">Create Quiz</h2>
+                                <div className='flex justify-between'>
+                                    <h2 className="text-xl font-bold text-foreground">Create Quiz</h2>
+                                    <button className="p-2 font-bold rounded-md bg-green-500">Publish Quiz</button>
+                                </div>
 
                                 <div>
                                     <label htmlFor="quizTitle" className="block text-sm font-medium text-foreground mb-1">Question Title</label>
                                     <input type="text" id="quizTitle" name="quizTitle"
                                         className="w-full mt-2 p-2 border border-input rounded-md bg-background text-foreground"
-                                        placeholder="Enter quiz title" />
+                                        placeholder="Enter quiz title"
+                                        value={singleQuestion.question}
+                                        onChange={(e) =>
+                                            setSingleQuestion((prev) => ({ ...prev, question: e.target.value }))
+                                        }
+                                    />
                                 </div>
 
                                 <p className="text-sm text-gray-600 mt-4">Add Options</p>
 
-                                <div id="optionsContainer" className="space-y-2 mt-4">
+                                {singleQuestion.options.map((option, idx) => (
                                     <div
-                                        className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-                                        <input type="checkbox" id="option0" name="correctAnswer" value="0"
-                                            className="text-primary focus:ring-0 w-4 h-4" />
-                                        <label htmlFor="option0" className="sr-only">Option 1</label>
-                                        <input type="text" id="optionText0" name="optionText0"
+                                        key={idx}
+                                        className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white"
+                                    >
+                                        <input
+                                            type="radio"
+                                            name="correctAnswer"
+                                            value={option}
+                                            checked={singleQuestion.correctAnswer === option}
+                                            onChange={() =>
+                                                setSingleQuestion((prev) => ({ ...prev, correctAnswer: option }))
+                                            }
+                                            className="text-primary focus:ring-0 w-4 h-4"
+                                        />
+                                        <input
+                                            type="text"
+                                            value={option}
+                                            onChange={(e) => {
+                                                const newOptions = [...singleQuestion.options];
+                                                newOptions[idx] = e.target.value;
+                                                setSingleQuestion((prev) => ({
+                                                    ...prev,
+                                                    options: newOptions,
+                                                }));
+                                            }}
                                             className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-                                            placeholder="Option 1" />
+                                            placeholder={`Option ${idx + 1}`}
+                                        />
                                     </div>
+                                ))}
 
-                                    {/** Option 2 */}
-                                    <div
-                                        className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-                                        <input type="checkbox" id="option2" name="correctAnswer" value="0"
-                                            className="text-primary focus:ring-0 w-4 h-4" />
-                                        <label htmlFor="option0" className="sr-only">Option 2</label>
-                                        <input type="text" id="optionText2" name="optionText2"
-                                            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-                                            placeholder="Option 2" />
-                                    </div>
-
-                                    {/** Option 2 */}
-                                    <div
-                                        className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-                                        <input type="checkbox" id="option3" name="correctAnswer" value="0"
-                                            className="text-primary focus:ring-0 w-4 h-4" />
-                                        <label htmlFor="option3" className="sr-only">Option 3</label>
-                                        <input type="text" id="optionText3" name="optionText3"
-                                            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-                                            placeholder="Option 3" />
-                                    </div>
-
-                                    {/** Option 4 */}
-                                    <div
-                                        className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white">
-                                        <input type="checkbox" id="option4" name="correctAnswer" value="0"
-                                            className="text-primary focus:ring-0 w-4 h-4" />
-                                        <label htmlFor="option4" className="sr-only">Option 4</label>
-                                        <input type="text" id="optionText4" name="optionText4"
-                                            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
-                                            placeholder="Option 4" />
-                                    </div>
-                                </div>
                                 <button
                                     className="w-full bg-primary text-white text-primary-foreground p-2 rounded-md hover:bg-primary/90 transition-colors">
                                     Save Quiz
@@ -107,102 +115,17 @@ export default function QuizSetEntryPage() {
 
                         {/** Right Column */}
                         <div className="px-4">
-                            {/** Question One */}
-                            <div className="rounded-lg overflow-hidden shadow-sm mb-4">
-                                <div className="bg-white p-6 !pb-2">
-                                    <div className="flex justify-between items-center mb-4">
-                                        <h3 className="text-lg font-semibold">
-                                            1. Which of the following is NOT a binary tree traversal method?
-                                        </h3>
-                                    </div>
-                                    <div className="space-y-2">
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer1" className="form-radio text-buzzr-purple" checked />
-                                            <span>Inorder</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer1" className="form-radio text-buzzr-purple" />
-                                            <span>Preorder</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer1" className="form-radio text-buzzr-purple" />
-                                            <span>Postorder</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer1" className="form-radio text-buzzr-purple" />
-                                            <span>Crossorder</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="flex space-x-4 bg-primary/10 px-6 py-2">
-                                    <button className="text-red-600 hover:text-red-800 font-medium">Delete</button>
-                                    <button className="text-primary hover:text-primary/80 font-medium">Edit Question</button>
-                                </div>
-                            </div>
-
-                            {/** Question Two */}
-                            <div className="rounded-lg overflow-hidden shadow-sm mb-4">
-                                <div className="bg-white p-6 !pb-2">
-                                    <div className="flex justify-between items-center mb-4">
-                                        <h3 className="text-lg font-semibold">
-                                            2. What is the maximum number of nodes at level &apos;L&apos; in a binary tree?
-                                        </h3>
-                                    </div>
-                                    <div className="space-y-2">
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer2" className="form-radio text-buzzr-purple" checked />
-                                            <span>2^L</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer2" className="form-radio text-buzzr-purple" />
-                                            <span>L</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer2" className="form-radio text-buzzr-purple" />
-                                            <span>2^(L-1)</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer2" className="form-radio text-buzzr-purple" />
-                                            <span>2L</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="flex space-x-4 bg-primary/10 px-6 py-2">
-                                    <button className="text-red-600 hover:text-red-800 font-medium">Delete</button>
-                                    <button className="text-primary hover:text-primary/80 font-medium">Edit Question</button>
-                                </div>
-                            </div>
-
-                            {/** Question 3 */}
-                            <div className="rounded-lg overflow-hidden shadow-sm mb-4">
-                                <div className="bg-white p-6 !pb-2">
-                                    <div className="flex justify-between items-center mb-4">
-                                        <h3 className="text-lg font-semibold">3. What is the height of an empty binary tree?</h3>
-                                    </div>
-                                    <div className="space-y-2">
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer3" className="form-radio text-buzzr-purple" checked />
-                                            <span>0</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer3" className="form-radio text-buzzr-purple" />
-                                            <span>-1</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer3" className="form-radio text-buzzr-purple" />
-                                            <span>1</span>
-                                        </label>
-                                        <label className="flex items-center space-x-3">
-                                            <input type="radio" name="answer3" className="form-radio text-buzzr-purple" />
-                                            <span>Undefined</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div className="flex space-x-4 bg-primary/10 px-6 py-2">
-                                    <button className="text-red-600 hover:text-red-800 font-medium">Delete</button>
-                                    <button className="text-primary hover:text-primary/80 font-medium">Edit Question</button>
-                                </div>
-                            </div>
+                            {adminQuestionsData?.map((questionData, idx) => (
+                                <AdminQuestionCard
+                                    key={idx}
+                                    index={questionData.index}
+                                    question={questionData?.question}
+                                    options={questionData?.options}
+                                    correctAnswer={questionData?.correctAnswer}
+                                    setSingleQuestion={setSingleQuestion}
+                                />
+                            ))
+                            }
                         </div>
                     </div>
                 </div>
