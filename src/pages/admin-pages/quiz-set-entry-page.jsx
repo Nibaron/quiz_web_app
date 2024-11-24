7fd9c024f1a28f67f971dd287f431d9ce6033460
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-11-24 17:05:37 +0600

    quiz add delete and update completed

--- src/pages/admin-pages/quiz-set-entry-page.jsx
+++ src/pages/admin-pages/quiz-set-entry-page.jsx
@@ -1,10 +1,11 @@
-import Sidebar from '../../components/admin-components/sidebar';
 import { useContext, useEffect, useState } from 'react';
-import { QuizIdContext } from '../../context';
+import { useForm } from 'react-hook-form';
+import Sidebar from '../../components/admin-components/sidebar';
 import AdminQuestionCard from '../../components/admin-components/admin-question-card';
+import { QuizIdContext } from '../../context';
 import useAxios from '../../hooks/useAxios';
+import Field from '../../components/common/fields';
 import { NextIcon } from '../../assets/icons';
-
 
 export default function QuizSetEntryPage() {
     const { quizId } = useContext(QuizIdContext);
@@ -13,91 +14,117 @@ export default function QuizSetEntryPage() {
     const [status, setStatus] = useState(null);
     const [refreshKey, setRefreshKey] = useState(0);
     const [isEditing, setIsEditing] = useState(false);
-    const [singleQuestion, setSingleQuestion] = useState({
-        "id": "",
-        "question": "",
-        "options": ["", "", "", ""],
-        "correctAnswer": "",
-    })
+
+    const defaultQuestion = {
+        id: '',
+        question: '',
+        options: ['', '', '', ''],
+        selectedOption: null,
+    };
+    const [singleQuestion, setSingleQuestion] = useState(defaultQuestion);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        watch,
+        setValue,
+    } = useForm({
+        defaultValues: defaultQuestion,
+    });
+
+    const selectedOption = watch('selectedOption');
 
     useEffect(() => {
+
         const fetchQuizData = async () => {
             try {
-                const { data, status } = await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/admin/quizzes`);
-
+                const { data, status } = await api.get(
+                    `${import.meta.env.VITE_SERVER_BASE_URL}/admin/quizzes`);
                 if (status === 200) {
                     const filteredData = data.find(quiz => quiz.id === quizId);
                     setAdminQuestionsData(filteredData);
                     setStatus(filteredData?.status);
                 }
             } catch (err) {
-                console.error("Error fetching quiz data:", err);
+                if (err.name !== 'AbortError') {
+                    console.error('Error fetching quiz data:', err);
+                }
             }
         };
         fetchQuizData();
     }, [api, quizId, refreshKey]);
 
-    const handleDelete = async (questionId) => {
+
+
+    useEffect(() => {
+        if (isEditing) {
+            reset(singleQuestion);
+        }
+    }, [singleQuestion, reset, isEditing]);
+
+    const handleDelete = async questionId => {
         try {
             const { status } = await api.delete(`${import.meta.env.VITE_SERVER_BASE_URL}/admin/questions/${questionId}`);
             if (status === 200) {
                 setRefreshKey(prev => prev + 1);
-                setSingleQuestion({
-                    "id": "",
-                    "question": "",
-                    "options": ["Option 1", "Option 2", "Option 3", "Option 4"],
-                    "correctAnswer": "option 1",
-                });
+                setSingleQuestion(defaultQuestion);
             }
         } catch (err) {
-            console.error("Error deleting quiz question:", err);
+            console.error('Error deleting quiz question:', err);
         }
     };
 
-    const handleCreateQuestion = async () => {
+    const submitForm = async formData => {
         try {
-            let url = `${import.meta.env.VITE_SERVER_BASE_URL}/admin/quizzes/${quizId}/questions`;
-            const { status } = await api.post(url, singleQuestion);
+            const selectedOption = formData.selectedOption;
+            const payload = {
+                options: formData.options,
+                question: formData.question,
+                correctAnswer: formData.options[selectedOption],
+            };
+
+            const url = `${import.meta.env.VITE_SERVER_BASE_URL}/admin/quizzes/${quizId}/questions`;
+            const { status } = await api.post(url, payload);
             if (status === 201) {
                 setRefreshKey(prev => prev + 1);
-                setSingleQuestion({
-                    "id": null,
-                    "question": "",
-                    "options": ["", "", "", ""],
-                    "correctAnswer": "#",
-                });
+                setSingleQuestion(defaultQuestion);
+                setIsEditing(false);
+                reset(defaultQuestion);
             }
         } catch (err) {
-            console.error("Error adding new quiz question:", err);
+            console.error('Error adding new quiz question:', err);
         }
-    }
+    };
 
-    const handleUpdateQuestion = async () => {
+    const handleUpdateQuestion = async formData => {
         try {
-            let url = `${import.meta.env.VITE_SERVER_BASE_URL}/admin/questions/${singleQuestion.id}`;
-            const { status } = await api.patch(url, singleQuestion);
+            const selectedOption = formData.selectedOption;
+            const payload = {
+                options: formData.options,
+                question: formData.question,
+                correctAnswer: formData.options[selectedOption],
+            };
+            const url = `${import.meta.env.VITE_SERVER_BASE_URL}/admin/questions/${singleQuestion.id}`;
+            const { status } = await api.patch(url, payload);
             if (status === 200) {
                 setRefreshKey(prev => prev + 1);
-                setSingleQuestion({
-                    "id": null,
-                    "question": "",
-                    "options": ["", "", "", ""],
-                    "correctAnswer": "#",
-                });
+                setSingleQuestion(defaultQuestion);
+                setIsEditing(false);
+                reset(defaultQuestion);
             }
         } catch (err) {
-            console.error("Error updating quiz question:", err);
+            console.error('Error updating quiz question:', err);
         }
-    }
-
-    //console.log(singleQuestion);
+    };
 
     return (
         <main className="bg-[#F5F3FF] min-h-screen flex">
             <Sidebar />
 
             <section className="md:flex-grow px-4 sm:px-6 lg:px-8 py-8">
-                <div >
+                <div>
                     <nav className="text-sm mb-4" aria-label="Breadcrumb">
                         <ol className="list-none p-0 inline-flex">
                             <li className="flex items-center">
@@ -111,107 +138,81 @@ export default function QuizSetEntryPage() {
                     </nav>
 
                     <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-8 lg:gap-12">
-                        {/** Left Column */}
-                        <div className="">
+                        {/* Left Column */}
+                        <form onSubmit={isEditing ? handleSubmit(handleUpdateQuestion) : handleSubmit(submitForm)}>
                             <h2 className="text-3xl font-bold mb-4">{adminQuestionsData?.title}</h2>
                             <div className="bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded-full inline-block mb-4">
-                                {`Total number of questions : ${adminQuestionsData?.Questions?.length || 0}`}
+                                {`Total number of questions: ${adminQuestionsData?.Questions?.length || 0}`}
                             </div>
-                            <p className="text-gray-600 mb-4 text-justify">
-                                {adminQuestionsData?.description}
-                            </p>
+                            <p className="text-gray-600 mb-4 text-justify">{adminQuestionsData?.description}</p>
 
-                            <form className="space-y-4" onSubmit={() => isEditing ? handleUpdateQuestion() : handleCreateQuestion()}>
+                            <div className="space-y-4">
                                 <div className='flex justify-between'>
                                     <h2 className="text-xl font-bold text-foreground">Create Quiz</h2>
                                     <button className="p-2 font-bold rounded-md bg-green-500">{status === 'draft' ? 'Publish Quiz' : 'Unpublish Quiz'}</button>
                                 </div>
-
-                                <div>
-                                    <label htmlFor="quizTitle" className="block text-sm font-medium text-foreground mb-1">Question Title</label>
-                                    <input type="text" id="quizTitle" name="quizTitle"
-                                        className="w-full mt-2 p-2 border border-input rounded-md bg-background text-foreground"
+                                <Field label="Question Title" error={errors.question}>
+                                    <input
+                                        {...register('question', { required: 'Question is Required' })}
+                                        className={`w-full mt-2 p-2 border rounded-md ${errors.question ? 'border-red-500' : 'border-gray-300'}`}
+                                        type="text"
+                                        name='question'
                                         placeholder="Enter quiz title"
-                                        value={singleQuestion.question}
-                                        onChange={(e) =>
-                                            setSingleQuestion((prev) => ({ ...prev, question: e.target.value }))
-                                        }
-                                        required
                                     />
-                                </div>
+                                </Field>
 
                                 <p className="text-sm text-gray-600 mt-4">Add Options</p>
 
-                                {singleQuestion.options.map((option, idx) => (
-                                    <div
-                                        key={idx}
-                                        className="flex items-center space-x-2 px-4 py-1 rounded-md group focus-within:ring focus-within:ring-primary/80 bg-white"
-                                    >
+                                {/* Error messages for selectedOption and options */}
+
+
+                                {singleQuestion.options.map((_, idx) => (
+                                    <div key={idx} className="flex items-center space-x-2 bg-white rounded-md px-4 py-1">
                                         <input
                                             type="radio"
-                                            name="correctAnswer"
-                                            value={option}
-                                            checked={singleQuestion.correctAnswer === option}
-                                            onChange={() =>
-                                                setSingleQuestion((prev) => ({ ...prev, correctAnswer: option }))
-                                            }
+                                            name='selectedOption'
+                                            id='selectedOption'
+                                            value={idx}
+                                            checked={selectedOption === idx}
+                                            onChange={() => setValue('selectedOption', idx)}
                                             className="text-primary focus:ring-0 w-4 h-4"
                                             required
                                         />
+
                                         <input
-                                            type="text"
-                                            value={option}
-                                            onChange={(e) => {
-                                                const newOptions = [...singleQuestion.options];
-                                                newOptions[idx] = e.target.value;
-                                                setSingleQuestion((prev) => ({
-                                                    ...prev,
-                                                    options: newOptions,
-                                                }));
-                                            }}
-                                            className="w-full p-2 bg-transparent rounded-md text-foreground outline-none focus:ring-0"
+                                            {...register(`options.${idx}`, { required: 'Option cannot be empty' })}
+                                            className="w-full p-2 bg-transparent text-foreground outline-none"
                                             placeholder={`Option ${idx + 1}`}
                                             required
                                         />
+
                                     </div>
                                 ))}
 
-
-                                <button
-                                    className="w-full bg-primary text-white text-primary-foreground p-2 rounded-md hover:bg-primary/90 transition-colors"
-                                    type='submit'
-                                >
-                                    Save Quiz
+                                <button type="submit" className="w-full bg-primary text-white p-2 rounded-md">
+                                    {isEditing ? 'Update Quiz' : 'Save Quiz'}
                                 </button>
-                            </form>
+                            </div>
+                        </form>
 
-
-                        </div>
-
-                        {/** Right Column */}
+                        {/* Right Column */}
                         <div className="max-h-screen h-full">
-                            <div className="h-[calc(100vh-50px)] overflow-y-scroll ">
-                                <div className="px-4">
-                                    {singleQuestion && adminQuestionsData?.Questions?.map((questionData, index) => (
-                                        <AdminQuestionCard
-                                            key={questionData?.id}
-                                            index={index + 1}
-                                            id={questionData?.id}
-                                            question={questionData?.question}
-                                            options={questionData?.options}
-                                            correctAnswer={questionData?.correctAnswer}
-                                            setSingleQuestion={setSingleQuestion}
-                                            onDelete={handleDelete}
-                                            setIsEditing={setIsEditing}
-                                        />
-                                    ))
-                                    }
-                                </div>
+                            <div className="h-[calc(100vh-50px)] overflow-y-scroll">
+                                {adminQuestionsData?.Questions?.map((questionData, index) => (
+                                    <AdminQuestionCard
+                                        key={questionData?.id}
+                                        index={index + 1}
+                                        questionData={questionData}
+                                        setSingleQuestion={setSingleQuestion}
+                                        onDelete={handleDelete}
+                                        setIsEditing={setIsEditing}
+                                    />
+                                ))}
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
         </main>
-    )
+    );
 }
