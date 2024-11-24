62cff238d228ef875f96c1802263d53b55400fb1
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2024-11-24 22:43:52 +0600

    publish unpublish implemented

--- src/pages/admin-pages/quiz-set-entry-page.jsx
+++ src/pages/admin-pages/quiz-set-entry-page.jsx
@@ -11,7 +11,7 @@ export default function QuizSetEntryPage() {
     const { quizId } = useContext(QuizIdContext);
     const { api } = useAxios();
     const [adminQuestionsData, setAdminQuestionsData] = useState([]);
-    const [status, setStatus] = useState(null);
+    const [quizStatus, setQuizStatus] = useState(null);
     const [refreshKey, setRefreshKey] = useState(0);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -45,7 +45,7 @@ export default function QuizSetEntryPage() {
                 if (status === 200) {
                     const filteredData = data.find(quiz => quiz.id === quizId);
                     setAdminQuestionsData(filteredData);
-                    setStatus(filteredData?.status);
+                    setQuizStatus(filteredData?.status);
                 }
             } catch (err) {
                 if (err.name !== 'AbortError') {
@@ -68,6 +68,9 @@ export default function QuizSetEntryPage() {
         try {
             const { status } = await api.delete(`${import.meta.env.VITE_SERVER_BASE_URL}/admin/questions/${questionId}`);
             if (status === 200) {
+                //after delete if no question remains we unpublish the quiz set
+                if (adminQuestionsData?.Questions?.length === 1) handlePublish();
+
                 setRefreshKey(prev => prev + 1);
                 setSingleQuestion(defaultQuestion);
             }
@@ -76,7 +79,7 @@ export default function QuizSetEntryPage() {
         }
     };
 
-    const submitForm = async formData => {
+    const submitForm = async (formData) => {
         try {
             const selectedOption = formData.selectedOption;
             const payload = {
@@ -98,7 +101,7 @@ export default function QuizSetEntryPage() {
         }
     };
 
-    const handleUpdateQuestion = async formData => {
+    const handleUpdateQuestion = async (formData) => {
         try {
             const selectedOption = formData.selectedOption;
             const payload = {
@@ -118,6 +121,25 @@ export default function QuizSetEntryPage() {
             console.error('Error updating quiz question:', err);
         }
     };
+
+    const handlePublish = async () => {
+        try {
+            const payload = {
+                "status": (quizStatus === 'draft') ? 'published' : 'draft',
+                "title": adminQuestionsData?.title
+            };
+            const url = `${import.meta.env.VITE_SERVER_BASE_URL}/admin/quizzes/${quizId}`;
+            const { status } = await api.patch(url, payload);
+            if (status === 200) {
+                setRefreshKey(prev => prev + 1);
+                setSingleQuestion(defaultQuestion);
+                setIsEditing(false);
+                reset(defaultQuestion);
+            }
+        } catch (err) {
+            console.error('Error updating quiz question:', err);
+        }
+    }
 
     return (
         <main className="bg-[#F5F3FF] min-h-screen flex">
@@ -139,7 +161,7 @@ export default function QuizSetEntryPage() {
 
                     <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-8 lg:gap-12">
                         {/* Left Column */}
-                        <form onSubmit={isEditing ? handleSubmit(handleUpdateQuestion) : handleSubmit(submitForm)}>
+                        <div>
                             <h2 className="text-3xl font-bold mb-4">{adminQuestionsData?.title}</h2>
                             <div className="bg-green-100 text-green-800 text-sm font-medium px-2.5 py-0.5 rounded-full inline-block mb-4">
                                 {`Total number of questions: ${adminQuestionsData?.Questions?.length || 0}`}
@@ -149,51 +171,54 @@ export default function QuizSetEntryPage() {
                             <div className="space-y-4">
                                 <div className='flex justify-between'>
                                     <h2 className="text-xl font-bold text-foreground">Create Quiz</h2>
-                                    <button className="p-2 font-bold rounded-md bg-green-500">{status === 'draft' ? 'Publish Quiz' : 'Unpublish Quiz'}</button>
+
+                                    <button className={`p-2 font-bold rounded-md ${quizStatus === 'draft' ? 'bg-green-300' : 'bg-red-300'} ${adminQuestionsData?.Questions?.length === 0 ? 'bg-gray-300 cursor-not-allowed' : ''}`} onClick={handlePublish} disabled={adminQuestionsData?.Questions?.length === 0} > {quizStatus === 'draft' ? 'Publish Quiz' : 'Unpublish Quiz'} </button>
                                 </div>
-                                <Field label="Question Title" error={errors.question}>
-                                    <input
-                                        {...register('question', { required: 'Question is Required' })}
-                                        className={`w-full mt-2 p-2 border rounded-md ${errors.question ? 'border-red-500' : 'border-gray-300'}`}
-                                        type="text"
-                                        name='question'
-                                        placeholder="Enter quiz title"
-                                    />
-                                </Field>
 
-                                <p className="text-sm text-gray-600 mt-4">Add Options</p>
+                                <form onSubmit={isEditing ? handleSubmit(handleUpdateQuestion) : handleSubmit(submitForm)}>
 
-                                {/* Error messages for selectedOption and options */}
-
-
-                                {singleQuestion.options.map((_, idx) => (
-                                    <div key={idx} className="flex items-center space-x-2 bg-white rounded-md px-4 py-1">
+                                    <Field label="Question Title" error={errors.question}>
                                         <input
-                                            type="radio"
-                                            name='selectedOption'
-                                            id='selectedOption'
-                                            value={idx}
-                                            checked={selectedOption === idx}
-                                            onChange={() => setValue('selectedOption', idx)}
-                                            className="text-primary focus:ring-0 w-4 h-4"
-                                            required
+                                            {...register('question', { required: 'Question is Required' })}
+                                            className={`w-full mt-2 p-2 border rounded-md ${errors.question ? 'border-red-500' : 'border-gray-300'}`}
+                                            type="text"
+                                            name='question'
+                                            placeholder="Enter quiz title"
                                         />
+                                    </Field>
 
-                                        <input
-                                            {...register(`options.${idx}`, { required: 'Option cannot be empty' })}
-                                            className="w-full p-2 bg-transparent text-foreground outline-none"
-                                            placeholder={`Option ${idx + 1}`}
-                                            required
-                                        />
+                                    <p className="text-sm text-gray-600 mt-4">Add Options</p>
 
-                                    </div>
-                                ))}
 
-                                <button type="submit" className="w-full bg-primary text-white p-2 rounded-md">
-                                    {isEditing ? 'Update Quiz' : 'Save Quiz'}
-                                </button>
+                                    {singleQuestion.options.map((_, idx) => (
+                                        <div key={idx} className="flex items-center space-x-2 bg-white rounded-md px-4 py-1 mt-4">
+                                            <input
+                                                type="radio"
+                                                name='selectedOption'
+                                                id='selectedOption'
+                                                value={idx}
+                                                checked={selectedOption === idx}
+                                                onChange={() => setValue('selectedOption', idx)}
+                                                className="text-primary focus:ring-0 w-4 h-4"
+                                                required
+                                            />
+
+                                            <input
+                                                {...register(`options.${idx}`, { required: 'Option cannot be empty' })}
+                                                className="w-full p-2 bg-transparent text-foreground outline-none"
+                                                placeholder={`Option ${idx + 1}`}
+                                                required
+                                            />
+
+                                        </div>
+                                    ))}
+
+                                    <button type="submit" className="w-full bg-primary text-white p-2 rounded-md mt-6">
+                                        {isEditing ? 'Update Quiz' : 'Save Quiz'}
+                                    </button>
+                                </form>
                             </div>
-                        </form>
+                        </div>
 
                         {/* Right Column */}
                         <div className="max-h-screen h-full">
